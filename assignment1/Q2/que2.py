ca09def7bf00b8390e0442267855aaccd02bb690
Jeevan Beniwal <jeevanbeniwal@gmail.com>
2019-07-16 13:53:10 +0530

    first commit

---
+++ assignment1/Q2/que2.py
@@ -0,0 +1,8 @@
+##############################################################################
+# Question 2:
+# Assuming you have a versioning enabled S3 bucket and multiple versions of the same object, 
+# Write a python script which takes the bucket name and object path as parameters 
+# and downloads the 2nd latest version of this object i.e the one prior to the latest version.
+##############################################################################
+
+
