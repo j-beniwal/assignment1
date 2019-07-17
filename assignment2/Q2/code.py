cbcda31f3a9e56c9c46e2b186297572f218264eb
Jeevan Beniwal <jeevanbeniwal@gmail.com>
2019-07-17 11:26:51 +0530

    added files

---
+++ assignment2/Q2/code.py
@@ -0,0 +1,6 @@
+############################################################################################
+# Develop a scalable architecture which does the resizing of an image uploaded by a userof an application
+# (ex:facebook; Resized Image to have a maximum dimension of 200px; 
+# Original Images are saved in S3; 
+# Resized images need to be saved in s3 with prefix "resized_")
+############################################################################################
